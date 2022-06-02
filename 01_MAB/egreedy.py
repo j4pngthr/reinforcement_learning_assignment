7b68b199bc961377bbde074b0f17088f068a6422
j4pngthr <weibeicun@gmail.com>
2022-06-02 14:01:02 +0900

    self.Q[i] = 0をinit_valにデバッグ

--- 01_MAB/egreedy.py
+++ 01_MAB/egreedy.py
@@ -2,35 +2,36 @@ import numpy as np
 import mab
 import bandit_algorithm
 
-class Agent(bandit_algorithm.BanditAlgorithm):
+class Agent(bandit_algorithm.BanditAlgorithm): # 強化学習のエージェント
     def __init__(self, env, epsilon, init_val):
         # The parameter for the environment
         self.env = env # env->main
         self.Q = np.zeros(env.k, dtype=np.float) # Expected values for each arm
-        self.N = np.zeros(env.k) # The number of trials for each arm
+        self.N = np.zeros(env.k) # そのレバーが引かれた回数
 
         # Algorithm parameters
         self.epsilon = epsilon # the epsilon value
         self.init_val = init_val # Initialization of the inital expected values
         if (init_val != 0):
             for i in range (0, len(self.Q)):
-                self.Q[i] = 0
+                self.Q[i] = init_val
 
     def action(self):
-        # Select an action
+        # Select an action # 確率で行動を決めるのか
         if (self.epsilon > np.random.rand()): # exploration
-            armid = np.random.randint(self.env.k)
+            arm_id = np.random.randint(self.env.k) # 一様分布で引くレバーを決める
         else: # exploit
             # The arm with the smallest index is selected when there are more than one optimal arms
-            armid = np.argmax(self.Q)
+            arm_id = np.argmax(self.Q)
 
         # Geting reward and incremental update
-        r = self.env.bandit(armid)
-        self.N[armid] += 1
-        if self.N[armid] == 1:
-            self.Q[armid] = r
+        r = self.env.bandit(arm_id)
+        self.N[arm_id] += 1 # そのレバーを引いた回数を+1
+        # そのレバーの価値Qを求める
+        if self.N[arm_id] == 1: # 初期値
+            self.Q[arm_id] = r
         else:
-            self.Q[armid] = self.Q[armid] + (r - self.Q[armid]) / self.N[armid] # Incremental update
+            self.Q[arm_id] = self.Q[arm_id] + (r - self.Q[arm_id]) / self.N[arm_id] # Incremental update
 
     def get_name(self):
         return "egreedy_" + str(self.epsilon) + "_" + str(self.init_val)
