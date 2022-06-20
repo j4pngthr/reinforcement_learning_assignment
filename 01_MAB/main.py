9d24d6a31031e432be9b15b7b8b79cd26464a91c
j4pngthr <weibeicun@gmail.com>
2022-06-20 11:35:19 +0900

    とりあえず動くものを作れた

--- 01_MAB/main.py
+++ 01_MAB/main.py
@@ -11,31 +11,41 @@ k = 10 # 1個のマシンの腕の数
 # sig = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 mu = np.empty(0)
 sig = np.empty(0)
-n_machine = 2000
+n_machine = 100
 
 init_val = 0
 
-def make_params(mu, sig): # parameters
+def make_params(): # parameters
+  global mu, sig
+  mu = np.empty(0)
+  sig = np.empty(0)
   for i in range(0, k):
-    global mu, sig
     mu = np.append(mu, random.gauss(0, 1))
     sig = np.append(sig, 1)
 
 # mab
 if __name__ == "__main__":
-  make_params();
-
   key = 0
   if key == 0:
     for epsilon in [0, 0.01, 0.1]:
-      # 毎回初期化しないとダメ
-      env = mab.MAB(k, mu, sig) # mab.py
-      a = egreedy.Agent(env, epsilon, init_val) # egreedy.py
+      for i in range(0, n_machine):
+        print(epsilon, "i", i)
+        # 毎回初期化しないとダメ
+        make_params()
+        env = mab.MAB(k, mu, sig) # mab.py
+        a = egreedy.Agent(env, epsilon, init_val) # egreedy.py
 
-      for t in range(0, env.MAX_STEPS):
-        for i in range(0, n_machine):
-
+        for t in range(0, env.MAX_STEPS):
+          # print("t", t)
           a.action() # egreedy.py/Agent.action
+
+        if i == 0:
+          avg_avg_rewards = np.empty(0)
+          for j in range(0, env.MAX_STEPS):
+            avg_avg_rewards = np.append(avg_avg_rewards, env.avg_rewards[j] / n_machine)
+        else:
+          for j in range(0, env.MAX_STEPS):
+            avg_avg_rewards[j] += env.avg_rewards[j] / n_machine
 
       # Output the results to a file
       # a.get_name() -> Agentの関数 egreedy.py
@@ -47,24 +57,24 @@ if __name__ == "__main__":
         else:
           file_name = file_name + _c
       file_name = "eps" + file_name + ".txt"
-      myio.write_data(a.get_name(), 0, env.avg_rewards.tolist(), file_name)
-  elif key == 1:
-    epsilon = 0.1
-    env = mab.MAB(k, mu, sig) # mab.py
-    # envの後
-    a = egreedy.Agent(env, epsilon, init_val)
-
-    for t in range(0, env.MAX_STEPS):
-      a.action() # egreedy.py/Agent.action
-
-    myio.write_data(a.get_name(), 0, env.avg_rewards.tolist(), "result.txt")
-  elif key == 2:
-    c = 1 # ucb
-    env = mab.MAB(k, mu, sig) # mab.py
-    a = ucb.Agent(env, epsilon, init_val) # egreedy.py
-
-    for t in range(0, env.MAX_STEPS):
-      a.action(t, c)
+      myio.write_data(a.get_name(), 0, avg_avg_rewards, file_name)
+  # elif key == 1:
+  #   epsilon = 0.1
+  #   env = mab.MAB(k, mu, sig) # mab.py
+  #   # envの後
+  #   a = egreedy.Agent(env, epsilon, init_val)
+  #
+  #   for t in range(0, env.MAX_STEPS):
+  #     a.action() # egreedy.py/Agent.action
+  #
+  #   myio.write_data(a.get_name(), 0, env.avg_rewards.tolist(), "result.txt")
+  # elif key == 2:
+  #   c = 1 # ucb
+  #   env = mab.MAB(k, mu, sig) # mab.py
+  #   a = ucb.Agent(env, epsilon, init_val) # egreedy.py
+  #
+  #   for t in range(0, env.MAX_STEPS):
+  #     a.action(t, c)
 
     # Output the results to a file
     # a.get_name() -> Agentの関数 egreedy.py
